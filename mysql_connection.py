4701f8506820ff8e6a00de0a9d208444c8f89979
KPKR1997 <krishnaprakash997@gmail.com>
2024-08-07 13:33:34 +0530

    sql2

--- mysql_connection.py
+++ mysql_connection.py
@@ -5,8 +5,8 @@ import streamlit as st
 
 conn = mysql.connector.connect(
     host = "127.0.0.1",
-    port = "3306",
-    user = "root@localhost",
+    port = 3306,
+    user = "root",
     password = "streamlit",
     db = "startup_funds"
 )
